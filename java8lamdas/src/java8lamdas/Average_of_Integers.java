7bca82d7fb217b85b91815bdb9386485d09ca5c3
Akhil <Akhil@LAPTOP-3O8A24HK>
2020-07-06 21:29:44 +0530

    commit

---
+++ java8lamdas/src/java8lamdas/Average_of_Integers.java
@@ -0,0 +1,20 @@
+package java8lamdas;
+
+
+import java.util.*; 
+import java.util.stream.IntStream; 
+
+public class Average_of_Integers {
+	 public static void main(String[] args) 
+	    {
+	        IntStream stream = IntStream.of(2, 3, 3, 4, 6, 7, 8); 
+	        OptionalDouble obj = stream.average(); 
+	        if (obj.isPresent()) { 
+	            System.out.println(obj.getAsDouble()); 
+	        } 
+	        else { 
+	            System.out.println("-1"); 
+	        } 
+	    } 
+
+}
